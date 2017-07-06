3e98009154825dd97fccef59b89b8d38b1fc0fd5
liweiyuan <535767595@qq.com>
2017-07-06 17:12:42 +0800

    commit

---
+++ src/main/java/com/tingyun/slipped/Lock.java
@@ -0,0 +1,28 @@
+package com.tingyun.slipped;
+
+
+
+public class Lock {
+    private boolean isLocked = true;
+
+    public void lock(){
+        synchronized(this){
+            while(isLocked){
+                try{
+                    this.wait();
+                } catch(InterruptedException e){
+                    //do nothing, keep waiting
+                }
+            }
+        }
+
+        synchronized(this){
+            isLocked = true;
+        }
+    }
+
+    public synchronized void unlock(){
+        isLocked = false;
+        this.notify();
+    }
+}
