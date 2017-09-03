677e70f2c4813a416f5a20e23bcb3b64ac1e0168
liweiyuan <535767595@qq.com>
2017-09-03 16:54:04 +0800

    add fork/join框架

---
+++ src/main/java/cn/concurrent/CountTask.java
@@ -0,0 +1,69 @@
+package cn.concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Created by spark on 17-9-3.
+ * Fork/Join框架
+ * RecursiveTask是有返回值的
+ */
+public class CountTask extends RecursiveTask<Long> {
+
+    private static final int THRESHOLD = 10000;
+    private long start;
+    private long end;
+
+    public CountTask(long start, long end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        long sum = 0;
+        boolean canCompute = (end - start) < THRESHOLD;
+        if (canCompute) {
+            for (long i = start; i < end; i++) {
+                sum += i;
+            }
+        } else {
+            //分成100小任务
+            long step = (start + end) / 100;
+            List<CountTask> subTasks = new ArrayList<CountTask>();
+            long post = start;
+            for (int i = 0; i < 100; i++) {
+                long lastOne = post + step;
+                if (lastOne > end) {
+                    lastOne = end;
+                }
+                CountTask subTask = new CountTask(post, lastOne);
+                post += step + 1;
+                subTasks.add(subTask);
+                subTask.fork();
+            }
+            for (CountTask t : subTasks) {
+                sum += t.join();
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool=new ForkJoinPool();
+        CountTask task=new CountTask(0,200000L);
+        ForkJoinTask<Long> result=forkJoinPool.submit(task);
+        try{
+            long res=result.get();
+            System.out.println("sum="+res);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
