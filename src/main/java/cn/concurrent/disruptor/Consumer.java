1850b0d0761b7e5f1e8ec6d063d2a7808e2b50ac
liweiyuan <535767595@qq.com>
2017-09-06 12:09:57 +0800

    and  disruptor

---
+++ src/main/java/cn/concurrent/disruptor/Consumer.java
@@ -0,0 +1,15 @@
+package cn.concurrent.disruptor;
+
+import com.lmax.disruptor.WorkHandler;
+
+/**
+ * Created by spark on 17-9-6.
+ * 消费者，作用是是读取数据进行处理
+ */
+public class Consumer implements WorkHandler<PCData> {
+    @Override
+    public void onEvent(PCData pcData) throws Exception {
+        System.out.println(Thread.currentThread().getId() +
+                ":Event:--" + pcData.get() * pcData.get() + "--");
+    }
+}
