9b4021012ae94b13111c5a6d81840061ed7c3947
liweiyuan <535767595@qq.com>
2017-09-18 23:20:58 +0800

    add web高并发

---
+++ src/main/java/cn/concurrent/task/SingleThreadRenderer.java
@@ -0,0 +1,24 @@
+package cn.concurrent.task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by spark on 17-9-18.
+ * <p>
+ * 串行渲染html页面
+ */
+public class SingleThreadRenderer {
+
+    void renderPage(CharSequence source) {
+        /*renderText(source);
+        List<?> imageData=new ArrayList<>();
+        for(ImageInfo imageInfo:scanForImageInfo(source)){
+            imageData.add(imageInfo.downLoadInfo());
+        }
+        for(ImageData data:imageData){
+            renderImage(data);
+        }*/
+
+    }
+}
