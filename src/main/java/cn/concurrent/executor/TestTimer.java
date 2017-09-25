25fa3b69c54688607f04aaea24069e68fafb2626
liweiyuan <535767595@qq.com>
2017-09-25 23:16:41 +0800

    add  Timer

---
+++ src/main/java/cn/concurrent/executor/TestTimer.java
@@ -0,0 +1,34 @@
+package cn.concurrent.executor;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by spark on 17-9-25.
+ */
+public class TestTimer {
+
+
+    static class Reminder {
+        Timer timer;
+
+        public Reminder(int sec) {
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("this is timer.");
+                    timer.cancel();
+                }
+            }, sec * 1000);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("------------------");
+        new Reminder(5);
+        System.out.println("..................");
+
+        System.out.println(8>>2);
+    }
+}
