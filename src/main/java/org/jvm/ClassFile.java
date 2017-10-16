1329b96e581355b72500c420e2cc6f1d980bf364
liweiyuan <535767595@qq.com>
2017-10-16 14:21:04 +0800

    add ClassFile解析

---
+++ src/main/java/org/jvm/ClassFile.java
@@ -0,0 +1,26 @@
+package org.jvm;
+
+/**
+ * Created by tingyun on 2017/10/16.
+ */
+public class ClassFile {
+    //Java虚拟机规范定义了u1、u2、u4和u8来分别表示1个字节、2个字节、4个字节和8个字节的无符号数
+    /**
+    public U4 magic;                            // magic
+    public U2 minorVersion;                     // minor_version
+    public U2 majorVersion;                     // major_version
+    public U2 constantPoolCount;                // constant_pool_count
+    public ConstantPoolInfo[] cpInfo;           // cp_info
+    public U2 accessFlags;                      // access_flags
+    public U2 thisClass;                        // this_class
+    public U2 superClass;                       // super_class
+    public U2 interfacesCount;                  // interfaces_count
+    public U2[] interfaces;                     // interfaces
+    public U2 fieldsCount;                      // fields_count
+    public FieldInfo[] fields;                  // fields
+    public U2 methodsCount;                     // methods_count
+    public MethodInfo[] methods;                // methods
+    public U2 attributesCount;                  // attributes_count
+    public BasicAttributeInfo[] attributes;     // attributes
+    */
+}
