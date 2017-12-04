92778ecbde8b1ee442d77a687fbd55cd06f693e4
liweiyuan <535767595@qq.com>
2017-12-04 10:42:45 +0800

    add CAS原理

---
+++ src/main/java/xx/yy/concurrent/cache/CasTest.java
@@ -0,0 +1,42 @@
+package xx.yy.concurrent.cache;
+
+/**
+ * Created by tingyun on 2017/12/4.
+ * 解释CAS原理:
+ * CAS有3个操作数，内存中的值V，预期内存中的值A，要修改成的值B。
+ * 当内存值V和预期值相同时，就将内存值V修改为B，否则什么都不做。
+ */
+public class CasTest implements Runnable {
+
+    private int memoryValue=1;
+    private int expectValue;
+    private int updateValue;
+
+    public CasTest(int expectValue, int updateValue) {
+        this.expectValue = expectValue;
+        this.updateValue = updateValue;
+    }
+
+    @Override
+    public void run() {
+        if(memoryValue==expectValue){
+            this.memoryValue=updateValue;
+            System.out.println("修改成功");
+        }else{
+            System.out.println("修改失败");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CasTest casTest1 = new CasTest(1,2);
+        Thread t1 = new Thread(casTest1);
+        t1.start();
+
+        Thread t2= new Thread(casTest1);
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+    }
+}
---
+++ src/main/java/xx/yy/concurrent/cache/FalseShare.java
@@ -0,0 +1,68 @@
+package xx.yy.concurrent.cache;
+
+/**
+ * Created by tingyun on 2017/12/1.
+ */
+public class FalseShare implements Runnable {
+    //线程数、数组大小：
+    public static int NUM_THREADS = 4; // change
+
+    //数组迭代的次数：
+    public final static long ITERATIONS = 500L * 1000L * 1000L;
+
+    //线程需要处理的数组元素角标：
+    private final int handleArrayIndex;
+
+    //操作数组：
+    private static VolatileLong[] longs = new VolatileLong[NUM_THREADS];
+
+    //对数组的元素进行赋值：
+    static{
+        for (int i = 0; i < longs.length; i++) {
+            longs[i] = new VolatileLong();
+        }
+    }
+
+    public FalseShare(final int handleArrayIndex) {
+        this.handleArrayIndex = handleArrayIndex;
+    }
+
+    //启动线程，每一个线程操作一个数组的元素，一一对应：
+    public static void main(final String[] args) throws Exception {
+        //程序睡眠必须加上：
+        Thread.sleep(10000);
+
+        final long start = System.nanoTime();
+
+        Thread[] threads = new Thread[NUM_THREADS];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new FalseShare(i));
+        }
+        for (Thread t : threads) {
+            t.start();
+        }
+        for (Thread t : threads) {
+            t.join();
+        }
+        System.out.println(System.nanoTime() - start);
+    }
+
+    //对数组的元素进行操作：
+    @Override
+    public void run() {
+        long i = ITERATIONS;
+        while (0 != --i) {
+            longs[handleArrayIndex].value = i;
+        }
+    }
+
+    //数组元素：
+    public final static class VolatileLong {
+        public volatile long value = 0L;
+        //public long p1, p2, p3, p4, p5; //代码1
+        //public int p6;//代码1
+    }
+    /**
+     * 注释代码1前后理解伪共享
+     */
+}
