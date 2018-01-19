5de93ab49dab12d387bf3b59a967eb46240172fe
liweiyuan <535767595@qq.com>
2018-01-19 14:23:32 +0800

    add 基于链表实现的栈

--- src/main/java/xx/yy/concurrent/algorithm/Stack.java
+++ src/main/java/xx/yy/concurrent/algorithm/Stack.java
@@ -1,42 +1,74 @@
 package xx.yy.concurrent.algorithm;
 
+import java.util.Iterator;
+
 /**
  * Created by tingyun on 2018/1/19.
  * 基于链表实现的下压栈
  */
-public class Stack<Item> {
+public class Stack<Item> implements Iterable<Item> {
 
     //定义栈顶元素
     private Node first;
     //元素数量
     private int size;
 
-    private class Node{
+    private class Node {
         private Item item;
         private Node next;
     }
 
     //判断栈是否空
-    public boolean isEmpty(){
-        return first==null;
+    public boolean isEmpty() {
+        return first == null;
     }
+
     //栈的深度
-    public int size(){
+    public int size() {
         return size;
     }
+
     //栈中添加一个元素
-    public  void push(Item item){
-        Node oldFirst=this.first;
-        Node first=new Node();
-        first.item=item;
-        first.next=oldFirst;
+    public void push(Item item) {
+        Node oldFirst = this.first;
+        Node first = new Node();
+        first.item = item;
+        first.next = oldFirst;
         size++;
     }
+
     //从栈中弹出一个元素
-    public Item pop(){
-        Item item=first.item;
-        first=first.next;
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
         size--;
         return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new StackIterator();
+    }
+
+    private class StackIterator<Item> implements Iterator<Item> {
+
+        private Node current = first;
+
+        @Override
+        public void remove() {
+
+        }
+
+        @Override
+        public boolean hasNext() {
+            return first == null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = (Item) current.item;
+            current = current.next;
+            return item;
+        }
     }
 }
