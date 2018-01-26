0f0d220c9cd060d1b061cb86267141ef878b238e
liweiyuan <535767595@qq.com>
2018-01-26 14:56:01 +0800

    add 计算m的n次方

--- src/main/java/xx/yy/concurrent/algorithm/Factorial.java
+++ src/main/java/xx/yy/concurrent/algorithm/Factorial.java
@@ -7,15 +7,31 @@ package xx.yy.concurrent.algorithm;
 public class Factorial {
 
     public static void main(String[] args) {
-        double result = calculate(3.0, 2);
+        double result = calculate(3.0, 3);
         System.out.println(result);
+        double power = power(3.0, 3);
+        System.out.println(power);
     }
 
+    //时间复杂度n*2
     private static double calculate(double d, int n) {
         double result = 1;
         for (int i = 0; i < n; i++) {
             result *= d;
         }
         return result;
+    }
+
+    //时间复杂度mlogn
+    private static double power(double d, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if ((n & 1) == 1) {
+            return d * power(d, n - 1);
+        } else {
+            double t = power(d, n / 2);
+            return t * t;
+        }
     }
 }
