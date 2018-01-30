137ac01129bcecc53399bffed3f2bfe65394bb67
liweiyuan <535767595@qq.com>
2018-01-30 22:28:59 +0800

    add  红黑树的实现

--- src/main/java/xx/yy/concurrent/algorithm/ReadBlackBST.java
+++ src/main/java/xx/yy/concurrent/algorithm/ReadBlackBST.java
@@ -8,13 +8,13 @@ public class ReadBlackBST<Key extends Comparable<Key>, Value> {
     //根节点
     private Node root;
 
-    private static final boolean RED=true;
-    private static final boolean BLACK=false;
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
 
-    private class Node{
+    private class Node {
         Key key;//键
         Value value;//相关联的值
-        Node left,right;//左右子树
+        Node left, right;//左右子树
         int N;//这个树中的节点总数
         boolean color;//由其父节点指向它的链接的颜色
 
@@ -25,11 +25,48 @@ public class ReadBlackBST<Key extends Comparable<Key>, Value> {
             this.color = color;
         }
     }
+
     //判断指向该节点的链接的颜色，约定空节点为黑色
-    public boolean isRed(Node x){
-        if (x==null){
+    public boolean isRed(Node x) {
+        if (x == null) {
             return false;
         }
-        return x.color==RED;
+        return x.color == RED;
     }
+
+    //左旋转--指将红色右链接转换
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = 1 + size(h.left) + size(h.right);
+        return x;
+    }
+
+    //将一个红色左链接转换为红色右链接
+    private Node rotateRight(Node h){
+        Node x=h.left;
+        h.left=x.right;
+        x.right=h;
+        x.color=h.color;
+        h.color=RED;
+        x.N=h.N;
+        h.N=1+size(h.left)+size(h.right);
+        return x;
+    }
+    private int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.N;
+        }
+    }
+
 }
