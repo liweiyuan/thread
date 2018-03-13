81f5737f87a591414b1a2daba907016ad2ad3a20
liweiyuan <535767595@qq.com>
2018-03-13 17:28:15 +0800

    cglib动态代理

--- src/main/java/xx/yy/concurrent/proxy/jdk/invoke/Client.java
+++ src/main/java/xx/yy/concurrent/proxy/jdk/invoke/Client.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Proxy;
 
 /**
  * Created by tingyun on 2018/3/13.
+ * 最重要的是newProxyInstance,这个方法中，指明了将要代理的类的加载器，
+ * 业务类接口，以及代理类要执行动作的调用处理器（InvokeHandler)
  */
 public class Client
 {
